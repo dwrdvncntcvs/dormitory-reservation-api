a99a46d1fd235df4b74f5ad023cf474297f0206b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-08-19 13:40:28 +0800

    Dormitory Rating Model Created

---
+++ models/dormrating.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class DormRating extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DormRating.init(
+    {
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      dormitoryId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "DormRating",
+    }
+  );
+  return DormRating;
+};
