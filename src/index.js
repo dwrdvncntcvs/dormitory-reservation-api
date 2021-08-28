97afbb0ea4ac427c520c602af550479da1ceafb6
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-08-28 16:05:10 +0800

    Routes added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const dormImageRoutes = require("./routes/dormImageRoutes");
 const reservationRoutes = require("./routes/reservationRoutes");
 const amenityRoutes = require("./routes/amenityRoutes");
 const dormRatingRoutes = require("./routes/dormRatingRoutes");
-const dormLocationRoutes = require("./routes/dormLocationRoutes")
+const dormLocationRoutes = require("./routes/dormLocationRoutes");
+const questionRoutes = require("./routes/questionRoutes");
 
 //Initializing express in variable app.
 const app = express();
@@ -22,7 +23,7 @@ const dormitory = [
   " ######   #####  ######  ##   ## ####### #######  #####  ######  ##   ## ",
   " ##   ## ##   ## ##   ## ### ###    #       #    ##   ## ##   ## ##   ## ",
   " ##   ## ##   ## ##   ## # ### #    #       #    ##   ## ##   ## ##   ## ",
-  " ##   ## ##   ## ######  #  #  #    #       #    ##   ## ######   #####   ",
+  " ##   ## ##   ## ######  #  #  #    #       #    ##   ## ######   #####  ",
   " ##   ## ##   ## ##   ## #  #  #    #       #    ##   ## ##   ##    #    ",
   " ######   #####  ##   ## #     # #######    #     #####  ##   ##    #    ",
 ];
@@ -42,6 +43,7 @@ app.use(reservationRoutes);
 app.use(amenityRoutes);
 app.use(dormRatingRoutes);
 app.use(dormLocationRoutes);
+app.use(questionRoutes);
 
 //To render the images using their paths.
 app.use("/image/profileImage", express.static("image/profileImage"));
