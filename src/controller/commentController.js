020e3c90f4a9bfb46ef13aa0baca481e4405224c
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-08-31 21:34:35 +0800

    Edit Comment Function and Endpoint Added

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -2,6 +2,7 @@ const db = require("../../models");
 const {
   findDormitoryData,
   findDormitoryQuestion,
+  findDormitoryComment,
 } = require("../database/find");
 
 exports.addComment = async (req, res) => {
@@ -29,15 +30,81 @@ exports.addComment = async (req, res) => {
     }
 
     await db.Comment.create({
-        comment,
-        commenter: userData.name,
-        role: userData.role,
-        questionId: questionData.id,
-        dormitoryId: dormitoryData.id,
-        userId: userData.id,
+      comment,
+      commenter: userData.name,
+      role: userData.role,
+      questionId: questionData.id,
+      dormitoryId: dormitoryData.id,
+      userId: userData.id,
     });
 
     return res.send({ msg: "Comment Added" });
+  } catch (err) {
+    console.log(err);
+    await t.rollback();
+    return res.status(500).send({ msg: "Something went wrong" });
+  }
+};
+
+exports.editComment = async (req, res) => {
+  const { comment, questionId, commentId, dormitoryId } = req.body;
+
+  const userData = req.user;
+  const questionData = await findDormitoryQuestion(questionId);
+  const commentData = await findDormitoryComment(commentId);
+  const dormitoryData = await findDormitoryData(dormitoryId);
+
+  const t = await db.sequelize.transaction();
+
+  try {
+    if (comment === "") {
+      await t.rollback();
+      return res.status(401).send({ msg: "Invalid Input" });
+    }
+
+    if (!dormitoryData) {
+      await t.rollback();
+      return res.status(404).send({ msg: "Dormitory not found" });
+    }
+
+    if (!questionData) {
+      await t.rollback();
+      return res.status(404).send({ msg: "Question not found" });
+    }
+
+    if (!commentData) {
+      await t.rollback();
+      return res.status(404).send({ msg: "Comment not found" });
+    }
+
+    if (commentData.dormitoryId !== dormitoryData.id) {
+      await t.rollback();
+      return res.status(404).send({ msg: "Comment not found" });
+    }
+
+    if (commentData.questionId !== questionData.id) {
+      await t.rollback();
+      return res.status(404).send({ msg: "Comment not found" });
+    }
+
+    if (commentData.userId !== userData.id) {
+      await t.rollback();
+      return res.status(404).send({ msg: "Comment not found" });
+    }
+
+    await db.Comment.update(
+      { comment },
+      {
+        id: commentData.id,
+        userId: userData.id,
+        questionId: questionData.id,
+        dormitoryId: dormitoryData.id,
+      },
+      { transaction: t }
+    );
+    await t.commit();
+
+    return res.send({ msg: "Comment Edited" });
   } catch (err) {
     console.log(err);
     await t.rollback();
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -8,4 +8,6 @@ const route = express.Router();
 
 route.post("/add-comment", requireAuth, commentController.addComment);
 
+route.put("/edit-comment", requireAuth, commentController.editComment);
+
 module.exports = route;
