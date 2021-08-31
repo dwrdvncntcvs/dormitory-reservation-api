b301b9ef84bf16c1abf60416088503f5927ab479
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-08-31 20:27:48 +0800

    Comment Model and Migration Changes

--- models/comment.js
+++ models/comment.js
@@ -41,6 +41,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       dormitoryId: {
         type: DataTypes.INTEGER,
         allowNull: false,
