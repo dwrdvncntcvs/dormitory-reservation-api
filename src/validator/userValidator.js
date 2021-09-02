e07ba21946bdbbcf686f573ac620f8d0240921d4
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-09-02 10:41:34 +0800

    User Validator Added

--- src/validator/userValidator.js
+++ src/validator/userValidator.js
@@ -58,7 +58,21 @@ exports.editAccountValidator = (toBeEdit, res, t) => {
 
 exports.is_roleValid = (validRole, res, t = null) => {
   if (t !== null) {
-    if (validRole === false) return res.status(401).send({ msg: "Invalid User" });
+    if (validRole === false)
+      return res.status(401).send({ msg: "Invalid User" });
   }
   if (validRole === false) return res.status(401).send({ msg: "Invalid User" });
+};
+
+exports.userValidator = (user, res, t = null) => {
+  if (t !== null) {
+    if (!user) {
+      t.rollback();
+      res.send({ msg: "Invalid User" });
+    }
+  }
+
+  if (!user) {
+    res.send({ msg: "Invalid User" });
+  }
 };
